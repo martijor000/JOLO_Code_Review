538a4c071dd9e36e1e78cb6c5fbe59c56b42fd85
Jordan Neal <jordaneal@gmail.com>
2022-05-23 11:39:06 -0700

    Methods added into Class

--- JOLO_FileManager/Class1.cs
+++ JOLO_FileManager/Class1.cs
@@ -2,6 +2,45 @@
 {
     public class Class1
     {
-
+        bool FileExists(/*filepath*/)
+        {
+            return true;
+        }
+        string DirectoryName(/*filepath*/)
+        {
+            return String.Empty;
+        }
+        string LargestFileInCurrentDirectory(/*filepath*/)
+        {
+            return String.Empty;
+        }
+        //      if a tie is found, first one alpha sorted
+        string VowelWeight(/*filepath*/)
+        {
+            return String.Empty;
+        }
+        //      Format: 12 Es, 1 A, 4 Is, 6 Os, 2 Us, 0Ys
+        //      return all zeros if file supplied has no txt extension
+        string FileName(/*filepath*/)
+        {
+            return String.Empty;
+        }
+        string FileExtension(/*filepath*/)
+        {
+            return String.Empty;
+        }
+        byte[]? GetByteArray(/*filepath*/)
+        {
+            return null;
+        }
+        string ToString()
+        {
+            return String.Empty;
+        }
+        //      returns a string concatenation of:
+        //          string FilePath
+        //          long Size
+        //          bool ReadOnly
+        //          DateTime DateChanged
     }
 }
