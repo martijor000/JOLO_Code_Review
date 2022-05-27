ad37c6628b1bbf6268b4549798a138bed1d253e7
Jordan Neal <jordaneal@gmail.com>
2022-05-27 01:04:47 -0700

    WINFORM

---
+++ TestHarness/SplashScreen.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace TestHarness
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+            this.LoadingBar = new System.Windows.Forms.ProgressBar();
+            this.tmrLoading = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // LoadingBar
+            // 
+            this.LoadingBar.BackColor = System.Drawing.Color.White;
+            this.LoadingBar.Location = new System.Drawing.Point(174, 488);
+            this.LoadingBar.Maximum = 200;
+            this.LoadingBar.Name = "LoadingBar";
+            this.LoadingBar.Size = new System.Drawing.Size(290, 20);
+            this.LoadingBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.LoadingBar.TabIndex = 0;
+            // 
+            // tmrLoading
+            // 
+            this.tmrLoading.Enabled = true;
+            this.tmrLoading.Interval = 20;
+            this.tmrLoading.Tick += new System.EventHandler(this.tmrLoading_Tick);
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(639, 610);
+            this.Controls.Add(this.LoadingBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SplashScreen";
+            this.Load += new System.EventHandler(this.SplashScreen_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ProgressBar LoadingBar;
+        private System.Windows.Forms.Timer tmrLoading;
+    }
+}
---
+++ TestHarness/SplashScreen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestHarness
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmrLoading_Tick(object sender, EventArgs e)
+        {
+            if (LoadingBar.Value < 200)
+            {
+                LoadingBar.Value += 2;
+            }
+            if (LoadingBar.Value == 200)
+            {
+                this.Close();
+            }
+        }
+
+    }
+}
